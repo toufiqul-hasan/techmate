c39657a8e06e9f714143b5f1288402ddd06943eb
toufiqul-hasan <toufiqul.shihab@gmail.com>
2023-07-29 04:08:03 +0600

    Monitor page added

--- pages/component/monitor.js
+++ pages/component/monitor.js
@@ -1,7 +1,72 @@
+import Link from "next/link";
+import Image from "next/image";
 import MainLayout from "../../layout/MainLayout";
 
-export default function monitor() {
+export default function Monitor({ data }) {
   return (
-    <MainLayout>monitor</MainLayout>
-  )
+    <MainLayout>
+      <div className="bg-primary1 text-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 pt-10">
+          <h1 className="font-semibold text-4xl lg:text-5xl text-center mb-2">
+            Monitor
+          </h1>
+          <h2 className="font-normal text-base lg:text-lg text-center mb-10">
+            Check & Get Your Desired Monitor!
+          </h2>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+            {data.map((item) => (
+              <div
+                key={item._id}
+                className="flex flex-col border border-white rounded-md shadow-md"
+              >
+                <div className="flex-1">
+                  <Image
+                    src={item.image}
+                    width={500}
+                    height={500}
+                    placeholder="blur"
+                    blurDataURL={item.image}
+                    alt={item.product_name}
+                    className="rounded-t-md"
+                  />
+
+                  <div className="px-4">
+                    <h2 className="font-medium text-xl my-2">
+                      {item.product_name}
+                    </h2>
+                    <p className="font-normal text-base">{item.category}</p>
+                    <p className="font-normal text-base"> ৳ {item.price}</p>
+                    <p className="font-normal text-base">{item.status}</p>
+                    <p className="font-normal text-base">
+                      {item.average_rating}
+                    </p>
+                  </div>
+                </div>
+
+                <div className="px-4">
+                  <Link href={`/`}>
+                    <button className="flex items-center justify-center w-full py-4 font-medium text-base rounded-md shadow-lg transition ease-in-out duration-200 my-5 bg-primary3 px-4 hover:bg-primary7 hover:text-primary1">
+                      Details
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
 }
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://techmatebd.onrender.com/product");
+  const data = await response.json();
+  const filteredData = data.filter((item) => item.category === "Monitor");
+  return {
+    props: {
+      data: filteredData,
+    },
+    revalidate: 10,
+  };
+};
