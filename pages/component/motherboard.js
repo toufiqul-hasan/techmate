8cd1202f8d091638fe67570fbaf69f958ce5221f
toufiqul-hasan <toufiqul.shihab@gmail.com>
2023-07-29 04:20:58 +0600

    Motherboard apge added

---
+++ pages/component/motherboard.js
@@ -0,0 +1,32 @@
+import MainLayout from "../../layout/MainLayout";
+import Product from "../../components/component/Product";
+
+export default function Motherboard({ data }) {
+  return (
+    <MainLayout>
+      <div className="bg-primary1 text-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 pt-10">
+          <h1 className="font-semibold text-4xl lg:text-5xl text-center mb-2">
+            Motherboard
+          </h1>
+          <h2 className="font-normal text-base lg:text-lg text-center mb-10">
+            Check & Get Your Desired Motherboard!
+          </h2>
+          <Product data={data} />
+        </div>
+      </div>
+    </MainLayout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://techmatebd.onrender.com/product");
+  const data = await response.json();
+  const filteredData = data.filter((item) => item.category === "Motherboard");
+  return {
+    props: {
+      data: filteredData,
+    },
+    revalidate: 10,
+  };
+};
