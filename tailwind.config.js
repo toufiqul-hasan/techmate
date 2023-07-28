6398973a80a05a401e1cb0fdb9af133c3f0ea4a9
toufiqul-hasan <toufiqul.shihab@gmail.com>
2023-07-28 21:10:57 +0600

    setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -6,6 +7,23 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'primary1': '#010116',
+      'primary2': '#050C29',
+      'primary3': '#152238',
+      'primary4': '#3D4861',
+      'primary5': '#67728C',
+      'primary6': '#C2CEEB',
+      'primary7': '#03AEF4',
+      'gray1': '#494848',
+      'gray2': '#636363',
+      'gray3': '#909090',
+      'gray4': '#B4B4B4',
+      'gray5': '#D4D4D4',
+      'gray6': '#E6E6E6',
+      'black': '#000000',
+      'white': '#FFFFFF',
+    },
     extend: {},
   },
   plugins: [],
