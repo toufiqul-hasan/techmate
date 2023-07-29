0bb71bc3190a8688953340cf28494b8d8b37ffa1
toufiqul-hasan <toufiqul.shihab@gmail.com>
2023-07-29 10:50:45 +0600

    google login added

--- pages/login.js
+++ pages/login.js
@@ -59,7 +59,14 @@ export default function Login() {
 
             <p className="text-white font-medium text-xl">OR</p>
 
-            <button className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+            <button
+              onClick={() =>
+                signIn("google", {
+                  callbackUrl: "http://localhost:3000/",
+                })
+              }
+              className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            >
               <svg
                 className="h-6 w-6 mr-2"
                 xmlns="http://www.w3.org/2000/svg"
